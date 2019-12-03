004ce8139a8c4713d5344db850596bc779dab586
vcruz <vcruz@viva.com.do>
2019-12-03 15:02:35 -0400

    Inserting header and a counter for schedules

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   const [semana,setSemana] = useState([]);
   let [count,setCount] = useState(0);
   let [ite,setIte] = useState(0);
+  let [quan,setQuan] = useState(14);
   var len = Object.keys(semana).length;
   
   const handleChange = (e) => {
@@ -22,10 +23,12 @@ function App() {
   //Agregando materias con sus dias y horarios
   const handleSubmit =  (e) => {
     e.preventDefault();
+    setQuan(quan - 1);
     setIte(ite + 1);
     var hora = ['9:00am - 1:00pm','2:00pm - 6:00pm','6:00pm - 10:00pm'];
     var dias = ['Lunes','Martes','Miercoles','Jueves','Viernes'];
     setSemana(semana.concat([{dia:dias[count],materia:input,hora:hora[ite]}]));
+    document.getElementById("counter").innerHTML = "Horarios disponibles: "+quan;
     if (ite === 2) {
       setIte(0);
     }
@@ -44,19 +47,23 @@ function App() {
     }
     else if(len === 15) {
       document.getElementById("FormRead").style.display = 'none';
+      document.getElementById("counter").style.display = 'none';
       alert('Ya no hay mas horarios disponibles');
     }
   }
 
   const Ended = () =>{
     document.getElementById("FormRead").style.display = 'none';
+    document.getElementById("counter").style.display = 'none';
     alert('Estos son los horarios del aula');
   }
 
   //renderizando componentes
   return (
     <div className="App">
-      <h2>Generador de horarios</h2>
+      <div className="header">
+        <h2>Generador de horarios</h2>
+      </div>
         <form id="FormRead" name="FormRead"  method="post" onSubmit={handleSubmit}>
             <input onChange={handleChange} name="materia" className="effect-1" type="text" id="infield" placeholder="Nombre de la materia" value={input}/>
             <div className="div_btn" id="div_btn">
@@ -64,11 +71,12 @@ function App() {
                 <button type="button" className="button2" id="btnfin" onClick={Ended}>Finalizar Horarios</button>
             </div>
         </form>
-        {len >= 1  ? <Lunes data={semana}/>     : ''}
-        {len >= 4  ? <Martes data={semana}/>    : ''}
+        <label id="counter">Horarios disponibles: 15</label>
+        {len >= 1  ? <Lunes     data={semana}/> : ''}
+        {len >= 4  ? <Martes    data={semana}/> : ''}
         {len >= 7  ? <Miercoles data={semana}/> : ''}
-        {len >= 10 ? <Jueves data={semana}/>    : ''}
-        {len >= 13 ? <Viernes data={semana}/>   : ''}
+        {len >= 10 ? <Jueves    data={semana}/> : ''}
+        {len >= 13 ? <Viernes   data={semana}/> : ''}
         {len >= 15 ? console.log(semana) : console.log('nada')}
         {console.log('Cantidad de materias:',Object.keys(semana).length)}
         {console.log('count:',count)}
